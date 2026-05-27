d539c5e172e6bd597118250f98aa5de3599ab158
marufbytes <marufahammed215@gmail.com>
2026-05-27 21:47:12 +0600

    dummy

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// This function opens and closes the mobile menu
+function toggleMenu() {
+  const menu = document.getElementById("mobileMenu");
+
+  // hidden class means menu is not showing
+  menu.classList.toggle("hidden");
+}
